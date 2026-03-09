6e751e62ef0712b5fc92596da862790d18b9b041
lilor159357 <166955096+lilor159357@users.noreply.github.com>
2026-03-09 19:07:19 +0200

    Update __init__.py

--- core/sources/__init__.py
+++ core/sources/__init__.py
@@ -2,5 +2,6 @@ from .apkmirror import APKMirrorSource
 from .aptoide import AptoideSource
 from .aurora import AuroraSource
 from .apkpure import APKPureSource
+from .apkpure_mobile import APKPureMobileSource
 from .github import GitHubSource
 from .registry import create_source, SOURCE_DEFINITIONS
